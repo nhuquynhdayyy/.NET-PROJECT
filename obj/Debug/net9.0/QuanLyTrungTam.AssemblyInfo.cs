6046516687b2f60af7e5fa76395dbf3350f95f70
NguyenNhuQuynh <102230099@sv1.dut.udn.vn>
2025-04-21 14:50:19 +0700

    fix Statistics of Enrollment

--- obj/Debug/net9.0/QuanLyTrungTam.AssemblyInfo.cs
+++ obj/Debug/net9.0/QuanLyTrungTam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyTrungTam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49c8aa425f5c52d14a5eefa9d67d44abdf1151e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6834326da33f50a77020322ec6dadbfb5c95590")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyTrungTam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyTrungTam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
