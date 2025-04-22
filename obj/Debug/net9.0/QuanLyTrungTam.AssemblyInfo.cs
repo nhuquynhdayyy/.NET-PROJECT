887077d9c7d11073e97150a276265ee62bafa978
NguyenNhuQuynh <102230099@sv1.dut.udn.vn>
2025-04-23 01:50:53 +0700

    fix admin revenue report

--- obj/Debug/net9.0/QuanLyTrungTam.AssemblyInfo.cs
+++ obj/Debug/net9.0/QuanLyTrungTam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyTrungTam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f27cd0830fb7a316011447048dc5e7bd0c528800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7334ea64e3b0abd3704f9cbbccb526a7be2ac6ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyTrungTam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyTrungTam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
