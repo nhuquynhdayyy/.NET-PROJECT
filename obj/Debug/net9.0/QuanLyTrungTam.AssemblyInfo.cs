3a491fec22a5a6864c3bc9bd2a70b566824ee820
NguyenNhuQuynh <102230099@sv1.dut.udn.vn>
2025-04-23 21:01:40 +0700

    Hoc vien chinh sua thong tin ca nhan

--- obj/Debug/net9.0/QuanLyTrungTam.AssemblyInfo.cs
+++ obj/Debug/net9.0/QuanLyTrungTam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyTrungTam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf66de15785f644ce660d6899e9aa1ccb8f359aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f8c4d3f8f504699bcda6461e828dca0c276469")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyTrungTam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyTrungTam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
